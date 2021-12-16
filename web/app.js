d9df37d7b423049196b631203d4fdc130d39514d
davesaldub <davesaldub@gmail.com>
2021-12-16 15:37:52 -0500

    added more functions

--- web/app.js
+++ web/app.js
@@ -2,11 +2,17 @@ var input = document.querySelector("#input");
 var output = document.querySelector("#output");
 
 async function convert(){
-    output.value = await eel.convertWord(input.value)();
+    output.value += `English to Pig Latin => Translated Word: ${await eel.convertWord(input.value)()}\n`;
+    output.value += "Enter another word to continue, or click the stop button\n\n";
 }
-
+async function stop(){
+    output.value = `Total number of words converted ${await eel.returnWords()()}\n`;
+}
+async function reverse(){
+    output.value += `Pig Latin to English => Translated Word: ${await eel.convertBack(input.value)()}\n`;
+    output.value += "Enter another word to continue, or click the stop button\n\n";
+}
 function initialize(){
-    input.value = "Enter a word: ";
-    output.value = "No output yet";
+    input.value = "Enter a word here: ";
 }
 initialize()
