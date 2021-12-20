718c2c0cd279acffa3d251817be0ab43874fff19
David Saldubehere <davesaldub@gmail.com>
2021-12-20 16:16:46 -0500

    finished and built

--- web/app.js
+++ web/app.js
@@ -4,13 +4,15 @@ var output = document.querySelector("#output");
 async function convert(){
     output.value += `English to Pig Latin => Translated Word: ${await eel.convertWord(input.value)()}\n`;
     output.value += "Enter another word to continue, or click the stop button\n\n";
+    initialize()
 }
 async function stop(){
-    output.value = `Total number of words converted ${await eel.returnWords()()}\n`;
+    output.value += `Total number of words converted ${await eel.returnWords()()}\n`;
 }
 async function reverse(){
     output.value += `Pig Latin to English => Translated Word: ${await eel.convertBack(input.value)()}\n`;
     output.value += "Enter another word to continue, or click the stop button\n\n";
+    initialize()
 }
 function initialize(){
     input.value = "Enter a word here: ";
